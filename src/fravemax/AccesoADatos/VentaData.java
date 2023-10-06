e120def1ff1d2780672e65de4ee2d07d3c413c66
Gabriel Lisandro Kamimura <glk.05@outlook.com>
2023-10-06 17:36:45 -0300

    222

--- src/fravemax/AccesoADatos/VentaData.java
+++ src/fravemax/AccesoADatos/VentaData.java
@@ -8,7 +8,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import jdk.nashorn.internal.ir.LiteralNode;
 
 public class VentaData {
 
@@ -53,7 +58,72 @@ public class VentaData {
         }
 
     }
-    
-    public void borrarVenti()
+
+    public void borrarVenti(int idVenta) {
+        String ims = "Update venta set estado=0 Where idVenta=? ";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(ims);
+            ps.setInt(1, idVenta);
+            int filar = ps.executeUpdate();
+
+            if (filar == 1) {
+                JOptionPane.showMessageDialog(null, "Venta borrada");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se pudo borrar la venta deseada");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR: No se pudo conectar con la base de datos!!");
+        }
+
+    }
+
+    public void modiVenta(Venta venttana) {
+        String ui = "Update venta set idCliente=?, fechaVenta=?"
+                + "Where idVenta=?";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(ui);
+            ps.setInt(1, venttana.getCliente().getIdCliente());
+            ps.setDate(2, Date.valueOf(venttana.getFechaVenta()));
+            ps.setInt(3, venttana.getIdVenta());
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Venta modificada correctamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "Venta No modificada correctamente");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR: No se pudo conectar la base de datos");
+        }
+
+    }
+
+    public List<Venta> lister() {
+        String quer = "select * from venta where estado=1";
+        List<Venta> vge = new ArrayList<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement(quer);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Venta vent = new Venta();
+                vent.setIdVenta(rs.getInt("idVenta"));
+                Cliente cliente = cd.buscarCliente(rs.getInt("idcliente"));
+                vent.setCliente(cliente);
+                vent.setFechaVenta(rs.getDate("fechaVenta").toLocalDate());
+                vent.setEstado(rs.getBoolean("estado"));
+
+                vge.add(vent);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR: No se pudo conectar con la base de datos ");
+        }
+
+        return lister();
+    }
 
 }
